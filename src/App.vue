63687db3024021313bf951ede319aa0589841265
rsashna <rashna@ryerson.ca>
2021-01-22 03:11:20 -0500

    added sass theme switch, removed vue example page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </div> -->
   <router-view />
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,7 +15,7 @@
   color: #2c3e50;
 }
 
-#nav {
+/* #nav {
   padding: 30px;
 }
 
@@ -26,5 +26,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// custom styles
+import './assets/css/styles.scss'
+
 createApp(App)
   .use(store)
   .use(router)
