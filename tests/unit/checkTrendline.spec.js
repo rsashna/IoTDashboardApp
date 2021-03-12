6d6e0d0c950d78e35bd6eba0a6173b70ab97e150
rsashna <rashna@ryerson.ca>
2021-03-11 23:07:48 -0500

    added unit tests, renamed files

--- tests/unit/checkTrendline.spec.js
+++ tests/unit/checkTrendline.spec.js
@@ -12,4 +12,13 @@ describe("trendLine.vue", () => {
     it("Test: Device Usage Trends prompt text should be visible", () => {
       expect(component.text()).to.have.string('Device Usage Trends');
     });
+    it("Test: Button 1 visible", () => {
+      expect(component.text()).to.have.string('Weekly Trend');
+    });
+    it("Test: Button 2 visible", () => {
+      expect(component.text()).to.have.string('Monthly Trend');
+    });
+    it("Test: Button 3 visible", () => {
+      expect(component.text()).to.have.string('Yearly Trend');
+    });
 });
